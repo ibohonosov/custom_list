009b55efe80fad69d1e33b6ba4ac7c6921c1a1e6
ibohonosov <ibohonosov@gmail.com>
2024-12-13 22:37:03 +0200

    create progect, add prime-menu

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config';
+import { definePreset } from '@primevue/themes';
+import Aura from '@primevue/themes/aura';
+import Menubar from 'primevue/menubar';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}'
+    }
+  }
+});
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset
+  }
+});
+app.component('app-menubar', Menubar);
+
+app.mount('#app')
